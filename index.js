0c197c43db5879d20bb61caf491fedbf40f9321b
schandrupatla <55516943+schandrupatla@users.noreply.github.com>
2021-07-08 11:08:15 -0700

    added client

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 
 require('dotenv').config();
+const cors = require("cors");
+const path = require("path");
 const express = require("express");
 const server = express();
+server.use(express.json());
+server.use(cors());
+server.use(express.static(path.join(__dirname,'client/')));
 console.log(process.env.USER);
 const PORT = process.env.PORT || 5000
+server.get("/",(req, res)=>{
+    res.sendFile(path.join(__dirname,'client/build','index.html'));
+})
 server.get("/api", (req, res)=>{
    res.json({message:"web43 is working and ready to serve"})
+})
+server.use((req, res)=>{
+    res.json({mesaage:"Sorry could not server"});
 })
 server.listen(PORT, ()=>{
     console.log(`I am listening at ${PORT}`);
